8230517f2f44a1c260ba992c71dd5b26100fd3f0
Amine <elouattar@gmail.com>
2018-02-24 14:06:25 +0000

    Amine's commit

--- app/src/main/java/com/hindou/smash/BinActivity.java
+++ app/src/main/java/com/hindou/smash/BinActivity.java
@@ -95,6 +95,7 @@ public class BinActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onRoutingFailure(RouteException e) {
         //comment
+        //Amine's comment
     }
 
     @Override
